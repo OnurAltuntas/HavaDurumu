75a4562039b40309b486ee51966ce082526580d7
Onur Altuntas <onuraltuntas50@gmail.com>
2019-08-11 18:07:47 +0300

    Google Api.

---
+++ app/src/main/java/com/example/havadurumu/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.havadurumu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+            var request= StringRequest(Request.Method.GET,"https://www.google.com",object :Response.Listener<String>{
+
+                override fun onResponse(response: String?) {
+                    Toast.makeText(this@MainActivity,"CEVAP"+response,Toast.LENGTH_LONG).show()
+                }
+
+            },object:Response.ErrorListener{
+                override fun onErrorResponse(error: VolleyError?) {
+
+                }
+
+            })
+
+            MySingleton.getInstance(this)?.addToRequestQueue(request)
+
+    }
+
+}
